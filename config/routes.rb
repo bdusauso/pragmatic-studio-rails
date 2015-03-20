55df63c5a30a1a993f33a2621939989968dd21af
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2015-03-20 16:00:05 +0100

    11.Forms: Editing Part I

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-get '/movies'     => 'movies#index'
-get '/movies/:id' => 'movies#show', as: 'movie'
+  get '/movies' => 'movies#index'
+  get '/movies/:id' => 'movies#show', as: 'movie'
+  get '/movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+
+  root :to => 'movies#index'
 
 end
