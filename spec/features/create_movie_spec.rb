8812d2146ae0324bef39214f27eafd8640805e35
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2015-04-02 09:48:55 +0200

    19.Validations: Part II

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Creating a new movie" do
+
   it "saves the movie and shows the new movie's details" do
     visit movies_url
 
@@ -24,4 +25,16 @@ describe "Creating a new movie" do
 
     expect(page).to have_text('New Movie Title')
   end
+
+  it "doesn't save the movie if it's invalid" do
+    visit new_movie_url
+
+    expect {
+        click_button 'Create Movie'
+    }.not_to change(Movie, :count)
+
+    expect(current_path).to eq(movies_path)
+    expect(page).to have_text('error')
+  end
+
 end
