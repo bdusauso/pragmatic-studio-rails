0045ed070d1391c92a2071ab5287d115cf225b2d
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2015-04-02 16:09:51 +0200

    21.One-to-Many: Models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325143340) do
+ActiveRecord::Schema.define(version: 20150402132607) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
